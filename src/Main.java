160d39af8e76b362234534fed361a67e3b5a1504
Mandrivia <antoine.philippe62@gmail.com>
2016-01-06 19:56:39 +0100

    terminé la gestion de l'exécution des tests

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 
 import java.io.File;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,124 +19,148 @@ import spoon.Launcher;
 
 public class Main {
 
-//	private static final String INPUT_PROCESSOR = "processors.TestProcessor";
-//	private static final String INPUT_DATASET = "C:\\Users\\kevin\\Desktop\\wk-spoon\\IntroClassJava-master\\dataset";
-//
-//	private static void launchSpoon(String projectPath) throws Exception {
-//
-//		String[] spoonArgs = { "-i", projectPath, "-p", INPUT_PROCESSOR, "-x"
-//
-//		};
-//
-//		Launcher.main(spoonArgs);
-//
-//	}
-
-//	private static List<String> findSourceFolder(String path) {
-//		File[] files = new File(path).listFiles();
-//		List<String> pathToSources = new ArrayList<String>();
-//		for (File f : files) {
-//			if (f.isDirectory() && !f.getName().equals("src"))
-//				pathToSources.addAll(findSourceFolder(f.getAbsolutePath()));
-//			else if (f.isDirectory())
-//				pathToSources.add(f.getAbsolutePath());
-//		}
-//
-//		return pathToSources;
-//	}
-
-	private static List<String> findTestFolder(String path) {
+	private static final String INPUT_PROCESSOR = "processors.TestProcessor";
+	private static final String INPUT_DATASET = ".." + File.separator + "IntroClassJava" + File.separator + "dataset";
+	private static final String WHITEBOX_TEST = "WhiteboxTest";
+	private static final String BLACKBOX_TEST = "BlackboxTest";
+			
+	
+	private static URLClassLoader classLoader;
+	
+	private static void launchSpoon(String projectPath) throws Exception{
+		
+		String[] spoonArgs = { "-i", projectPath,
+				"-p", INPUT_PROCESSOR, "-x"
+		};
+		
+		Launcher.main(spoonArgs);
+		
+	}
+	
+	private static List<String> findTestFolder(String path, String color) {
 		File[] files = new File(path).listFiles();
 		List<String> pathToSources = new ArrayList<String>();
 		for (File f : files) {
 			if (f.isDirectory())
-				pathToSources.addAll(findTestFolder(f.getAbsolutePath()));
-			else if (f.getName().endsWith(".java"))
+				pathToSources.addAll(findTestFolder(f.getAbsolutePath(), color));
+			else if (f.getName().endsWith(color + ".java"))
 				pathToSources.add(f.getAbsolutePath());
 		}
 
 		return pathToSources;
 	}
 	
-	private static void convertToClassName(List<String> testFolder,String projetPath) {
-		for(int i =0;i<testFolder.size();i++){
-			String classe = testFolder.get(i);
-			classe  = classe.replace(projetPath, "").replace('\\', '.').replace(".java", "");
-			testFolder.remove(i);
-			testFolder.add(i,classe);
+	private static String convertToClassName(String file) {
+		int ind = file.split(File.separator).length;
+		return file.split(File.separator)[ind-2] + "." + file.split(File.separator)[ind -1].replace(".java", "");
+	}
+	
+	private static List<String> findSourceFolder(String path){
+		File[] files = new File(path).listFiles();
+		List<String> pathToSources = new ArrayList<String>();
+		for(File f : files){
+			if(f.isDirectory() && !f.getName().equals("src"))
+				pathToSources.addAll(findSourceFolder(f.getAbsolutePath()));
+			else if(f.isDirectory())
+				pathToSources.add(f.getAbsolutePath());
+		}
+		
+		return pathToSources;
+	}
+	
+	private static List<String> findJavaFiles(String path){
+		File[] files = new File(path).listFiles();
+		List<String> pathToSources = new ArrayList<String>();
+		for(File f : files){
+			if(f.isDirectory())
+				pathToSources.addAll(findJavaFiles(f.getAbsolutePath()));
+			else if(f.getName().endsWith(".java"))
+				pathToSources.add(f.getAbsolutePath());
+		}
+		
+		return pathToSources;
+	}
+	
+	private static void deleteClassFiles(String path){
+		File[] files = new File(path).listFiles();
+		for(File f : files){
+			if(!f.isDirectory() && f.getName().contains(".class"))
+				f.delete();
+			else if(f.isDirectory())
+				deleteClassFiles(f.getAbsolutePath());
 		}
 		
 	}
 
-	public static void main(String[] args) throws Exception {
-		String projetPath = "C:\\Users\\kevin\\Desktop\\wk-spoon\\prjTest\\src\\test\\java\\";
-		List<String> testFolder = findTestFolder(projetPath);
-		convertToClassName(testFolder, projetPath);
+	
+	public static Class<?> compile(String sourcePath) throws MalformedURLException{
+		JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+		
+		List<String> listSourceFiles = findJavaFiles(sourcePath + "/main/java");
+		List<String> listTestFiles = findJavaFiles(sourcePath + "/test/java");
 
+		for(String file : listSourceFiles){
+				compiler.run(null, null, null, file);
+//				System.out.println(file);
+		}
+		for(String file : listTestFiles)
+			compiler.run(null, null, null, "-cp", sourcePath + "/main/java:../junit4-4.11.jar", file);
+		
+//		System.out.println(sourcePath);
+		classLoader = URLClassLoader.newInstance(new URL[] {
+				new File(sourcePath + "/main/java/").toURI().toURL(), new File(sourcePath +"/test/java/").toURI().toURL()
+		});
+		
+		Class<?> classe = null;
+		
+	      try {
+	  		for(String file : listSourceFiles)
+	    		  Class.forName(convertToClassName(file), true, classLoader);
+	  		for(String file : listTestFiles)
+	  			if(file.contains("Whitebox"))
+	  				classe = Class.forName(convertToClassName(file), true, classLoader);
+
+	  		
+//	          System.out.println("DOOOOOOOOOONE");
+	       }
+	       catch(Exception e) {
+	    	   e.printStackTrace();
+	          System.out.println("Impossible d'instancier la classe");
+	       }
+	      
+	      return classe;
+		
+	}
+	
+	public static List<Failure> runTests(Class<?> testClass){
 		JUnitCore junit = new JUnitCore();
-
-		for (String test : testFolder) {
-
-			Class<?> classe = null;
-			try {
-
-				classe = Class.forName(test);
-				Result results = junit.run(classe);
-				System.out.println("Test run: "+test+" nbr fail:"+results.getFailureCount()+"/"+results.getRunCount());
-				System.out.println("fail: ");
-				for (Failure fail : results.getFailures()) {
-					System.out.println();
-					System.out.println(fail.getTestHeader());
-					System.out.println(fail.getMessage());
-					System.out.println();
-
-				}
-				System.out.println("Fin Test run: "+test);
-				System.out.println();
-				
-			} catch (Exception e) {
-				System.out.println("Impossible d'instancier la classe");
-			}
-
+		Result results = junit.run(testClass);
+		for(Failure failure : results.getFailures()){
+			System.out.println(failure);
+		}
+		return results.getFailures();
+	}
+	
+	public static void main(String[] args) throws Exception {
+		
+		long start = System.currentTimeMillis();
+		
+		deleteClassFiles(INPUT_DATASET);
+		System.out.println("=== Cleaned previous compiled class files === ");
+		List<String> sourceFolders = findSourceFolder(INPUT_DATASET);
+		Class<?> classe;
+		
+		for(String folder : sourceFolders){
+			classe = compile(folder);
+			runTests(classe);
 		}
 
-		// String sourceFilePath =
-		// "C:\\Users\\kevin\\Desktop\\wk-spoon\\IntroClassJava\\dataset\\checksum\\"
-		// +
-		// "2c1556672751734adf9a561fbf88767c32224fca14a81e9d9c719f18d0b21765038acc16ecd8377f74d4f43e8c844538161d869605e3516cf797d0a6a59f1f8e"
-		// +
-		// "\\003\\src\\main\\java\\introclassJava\\checksum_2c155667_003.java";
-		// File f = new File(sourceFilePath);
-		// System.out.println(sourceFilePath);
-		// System.out.println(f.getPath());
-		//// System.setProperty("java.home", "C:\\Program
-		// Files\\Java\\jdk1.7.0_75"); //to change java home if misconfiguration
-		// of eclipse
-		// System.out.println(System.getProperty("java.home"));
-		// JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-		// int result = compiler.run(System.in, System.out, System.err,
-		// f.getPath());
-		// File sourceFolder = new
-		// File("C:\\Users\\kevin\\Desktop\\wk-spoon\\IntroClassJava\\dataset\\checksum\\"
-		// +
-		// "2c1556672751734adf9a561fbf88767c32224fca14a81e9d9c719f18d0b21765038acc16ecd8377f74d4f43e8c844538161d869605e3516cf797d0a6a59f1f8e"
-		// + "\\003\\src\\main\\java\\introclassJava");
-		// URLClassLoader classLoader = URLClassLoader.newInstance(new URL[] {
-		// new
-		// File("C:\\Users\\kevin\\Desktop\\wk-spoon\\IntroClassJava\\dataset\\checksum\\"
-		// +
-		// "2c1556672751734adf9a561fbf88767c32224fca14a81e9d9c719f18d0b21765038acc16ecd8377f74d4f43e8c844538161d869605e3516cf797d0a6a59f1f8e"
-		// +
-		// "\\003\\src\\main\\java\\introclassJava\\checksum_2c155667_003.class").toURI().toURL()
-		// });
-
-		// List<String> list = findSourceFolder(INPUT_DATASET);
-		// for(String s : list){
-		// System.out.println("Launching Spoon on " + s.split("\\\\")[6] + " " +
-		// s.split("\\\\")[7]+ " " + s.split("\\\\")[8]);
-		// launchSpoon(s);
-		// }
+		System.out.println("\n\nTime taken : " + (System.currentTimeMillis() - start) / 1000 + " sec");
+				
+//		List<String> list = findSourceFolder(INPUT_DATASET);
+//		for(String s : list){
+//			System.out.println("Launching Spoon on " + s.split("\\\\")[6] + " " + s.split("\\\\")[7]+ " " +  s.split("\\\\")[8]);
+//			launchSpoon(s);
+//		}
 	}
-
-
 }
