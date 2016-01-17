5327061381da5a521ea39c176cbf0cdaf0099832
Antoine PHILIPPE <antoine.philippe62@gmail.com>
2016-01-17 23:01:43 +0100

    modif intProco

--- src/processors/IntMutatorProcessor.java
+++ src/processors/IntMutatorProcessor.java
@@ -6,19 +6,18 @@ import java.util.List;
 import java.util.Map;
 
 import parameters.IntValues;
+import parameters.NumericLiteralFilter;
 //import parameters.NumericLiteralFilter;
 import spoon.processing.AbstractProcessor;
-import spoon.reflect.code.BinaryOperatorKind;
-import spoon.reflect.code.CtAssignment;
 import spoon.reflect.code.CtBinaryOperator;
-import spoon.reflect.code.CtExpression;
+import spoon.reflect.code.CtFieldRead;
 import spoon.reflect.code.CtLiteral;
+import spoon.reflect.code.CtVariableRead;
 import spoon.reflect.declaration.CtClass;
-import spoon.reflect.factory.Factory;
+import spoon.reflect.declaration.CtElement;
 import spoon.reflect.visitor.Query;
 import spoon.reflect.visitor.filter.TypeFilter;
 import spoon.support.reflect.code.CtBinaryOperatorImpl;
-import spoon.support.reflect.code.CtLiteralImpl;
 
 public class IntMutatorProcessor extends AbstractProcessor<CtClass<?>> {
 	private static int intMutationPosition; //position de la variable à muter dnas la classe
@@ -39,11 +38,18 @@ public class IntMutatorProcessor extends AbstractProcessor<CtClass<?>> {
 	private static List<Integer> listOperators = new ArrayList<Integer>();
 	
 	private List<CtLiteral<Number>> literals = new ArrayList<CtLiteral<Number>>();
-	private static List<CtAssignment<?,?>> variables = new ArrayList<CtAssignment<?,?>>();
+	private static List<CtFieldRead<?>> variables = new ArrayList<CtFieldRead<?>>();
 	
 	static {
-		listOperators.add(-1);
-		listOperators.add(1);
+		listOperators.add(IntValues.NEGATIVE.getValue());
+		listOperators.add(IntValues.POSITIVE.getValue());
+//		listOperators.add(+10);
+//		listOperators.add(-10);
+//		listOperators.add(+100);
+//		listOperators.add(-100);
+//		listOperators.add(IntValues.MAX_INT.getValue());
+//		listOperators.add(IntValues.MIN_INT.getValue());
+//		listOperators.add(IntValues.ZERO.getValue());
 	}
 
 	public void init(){
@@ -63,37 +69,43 @@ public class IntMutatorProcessor extends AbstractProcessor<CtClass<?>> {
 				intValuePosition = 0;
 				prevValue = null;
 //				literals = Query.getElements(c, new TypeFilter<CtLiteral<Integer>>(CtLiteral.class));
-//				literals = Query.getElements(c, new NumericLiteralFilter());
-				variables = Query.getElements(c, new TypeFilter<CtAssignment<?,?>>(CtAssignment.class));
+				literals = Query.getElements(c, new NumericLiteralFilter());
+//				variables = Query.getElements(c, new TypeFilter<CtFieldRead<?>>(CtFieldRead.class));
 //				System.out.println(literals);
-				System.out.println(variables);
-				Factory f = getFactory();
-				for(CtAssignment<?,?> l : variables){
-					if(l.getType().getSimpleName().equals("int")){
+//				System.out.println(variables);
+//				Factory f = getFactory();
+//				for(CtVariableRead<?> l : variables){
+//					if(l.getType().getSimpleName().equals("int") && !l.toString().contains("args")){
 //						l.setAssignment(new CtLiteralImpl().setValue(l.getAssignment() + "+" + 1));
 //						l.getParent().setFactory(getFactory().Field().createReference((CtField<?>) l.getAssigned()));
 //						c.addField((CtField<?>) l.getAssigned());
-//						CtCodeSnippetExpression e = new CtCodeSnippetExpressionImpl(); 
-//						e.setValue("scanner.nextFloat()");
+//						CtCodeSnippetExpression<Number> e = new CtCodeSnippetExpressionImpl<Number>(); 
+//						e.setValue("2");
 //						CtBinaryOperator<?> b = new CtBinaryOperatorImpl<>();
-//						b.setLeftHandOperand(l.getAssignment());
+//						b.setLeftHandOperand(l);
 //						b.setKind(BinaryOperatorKind.PLUS);
-//						CtLiteral<Number> cl = new CtLiteralImpl<>();
+//						CtLiteral<Number> cl = new CtLiteralImpl<Number>();
 //						cl.setValue(1);
 //						b.setRightHandOperand(cl);
 //						l.setAssignment((CtExpression<?>) b);
-//						System.out.println(b.getSignature());
+//						System.out.println(b.getSignature());s
 //						l.setAssignment((CtExpression) e);
-//						System.out.println(l);
+//						l.setAssignment((CtExpression<?>) cl);
+//						CtFieldRead<?> field = new CtFieldReadImpl<Number>();
+//						field.set
+//						CtElement e = l.getParent();
+//						e = b.getParent();
+//						System.out.println(e.);
 //						CtExpression<?> e = l.getAssignment();
 						
-					}
+//					}
 						
 //					System.out.println(l.getAss);
-				}
+//				}
+//				System.exit(0);
 				IntMutatorProcessor.currentClass = this.getClass().getSimpleName();
 			}
-//			mutate();
+			mutate();
 			
 		}
 	}
@@ -122,12 +134,18 @@ public class IntMutatorProcessor extends AbstractProcessor<CtClass<?>> {
 				System.out.println(">>>3");
 				value = literals.get(intMutationPosition);
 			}
-			System.out.print(value.getValue() + " ====> ");
-			prevValue = value.getValue();
-			
-			value.setValue(value.getValue().intValue() + listOperators.get(intValuePosition).intValue());
-			System.out.println(value.getValue());
+			if(!value.getParent().toString().contains("args") && value.getValue().intValue() != (value.getValue().intValue() + listOperators.get(intValuePosition).intValue())){
+				System.out.print(value.getValue() + " ====> ");
+				prevValue = value.getValue();
+				if(listOperators.get(intValuePosition).intValue() == 0 || listOperators.get(intValuePosition).intValue() == Integer.MAX_VALUE || listOperators.get(intValuePosition).intValue() == Integer.MIN_VALUE){
+					value.setValue(listOperators.get(intValuePosition).intValue());
+				} else {
+					value.setValue(value.getValue().intValue() + listOperators.get(intValuePosition).intValue());
+				}
+				System.out.println(value.getValue());
+			}
 			System.out.println("POSITIONS : \nMutationPosition : " + intMutationPosition + " \nValuePosition : " + intValuePosition);
+
 			if(intValuePosition >= listOperators.size()-1 && intMutationPosition < literals.size()-1){
 				intMutationPosition++;
 				intValuePosition = 0;
